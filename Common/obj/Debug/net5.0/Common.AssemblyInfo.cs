519abf1e60d6d683dfd032fe91cbaa309a7f8d6f
kalledk21 <kaspermho@gmail.com>
2021-12-08 14:43:39 +0100

    New package version attempt

--- Common/obj/Debug/net5.0/Common.AssemblyInfo.cs
+++ Common/obj/Debug/net5.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorCommon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-test")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorCommon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StorCommon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
